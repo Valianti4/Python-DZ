789f9d8a565e07b113ea753fcf282d70dccb6f3a
Valianti4 <109241884+Valianti4@users.noreply.github.com>
2022-09-30 01:32:52 +0400

    Решил задачу 4

--- Practice02/Example04.py
+++ Practice02/Example04.py
@@ -4,20 +4,24 @@
 Позиции хранятся в файле file.txt в одной строке одно число.
 '''
 
+from random import randint
 
 print('Введите целое число.')
 n = int(input())
 list = [str(i) for i in range(-n, n + 1)]
 print(f'Последовательность: {list}')
-
-f = open('d:\Testirovshik\Python\DZ\Practice02\Python_Example04.txt', 'w')
-for i in list([0], [2], [5]):
-    f.write(i + '\n')
-f.close()
-# with open('d:\Testirovshik\Python\DZ\Practice02\Python_Example04.txt', "r") as f:
-#     for line in f:
-#         print(line.strip())
-# res = [str(i) for i in range(-n, n + 1)]
-# res = map(int, res)
-# import math
-# print(f'Произведение элементов списка: {math.prod(res)}')
+list2 = []
+for i in range(-n, n + 1):
+    list2.append(randint(-n, n))
+list3 = []
+for i in list2:
+    if not i in list3:
+        list3.append(i)
+file = open('d:\Testirovshik\Python\DZ\Practice02\Python_Example04.txt', 'w')
+for i in list3:
+    file.write(str(i) + '\n')
+file.close()
+with open('d:\Testirovshik\Python\DZ\Practice02\Python_Example04.txt', "r") as file:
+    for line in file:
+        print(line.strip())
+print(f'Произведение выбранных элементов списка: {list3[0] * list3[1] * list3[2]}.')
