1b443312513d0fbd4ce1d9012d0c48d7981bc3ec
Valianti4 <109241884+Valianti4@users.noreply.github.com>
2022-10-04 06:39:04 +0400

    Решил задачу 2

---
+++ Practice04/Example02.py
@@ -0,0 +1,23 @@
+'''
+Задача 2. Задайте натуральное число N. 
+Напишите программу, которая составит список простых множителей числа N.
+'''
+
+
+print('Чтобы задать список, введите число.')
+num = int(input())
+
+def mult_elements(num):
+    i = 2
+    list = []
+    a = num
+    while i <= num:
+        if num % i == 0:
+            list.append(i)
+            num //= i
+            i = 2
+        else:
+            i += 1
+    print(f'Список простых множителей числа {a}: {list}.')
+
+mult_elements(num)
