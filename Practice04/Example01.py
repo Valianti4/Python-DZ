c1182e757234fcc29d1330b80cf2cfad2d0186ed
Valianti4 <109241884+Valianti4@users.noreply.github.com>
2022-10-04 17:21:59 +0400

    Решил задачу 1

--- Practice04/Example01.py
+++ Practice04/Example01.py
@@ -4,3 +4,20 @@
 при d = 0.001, π = 3.141    10^(-1) ≤ d ≤10^(-10)
 '''
 
+
+print('Чтобы вычислить число c заданной точностью d, введите d:')
+d = float(input())
+
+def exas_pi(exas=1.0e-5):
+    s = 0
+    d = 1
+    sgn = 1
+    while True:
+        a = 4 / d
+        s = s + sgn * a
+        if a < exas:
+            return s
+        sgn = -sgn
+        d = d + 2
+ 
+print(f'Число c заданной точностью d равно {round((exas_pi()), 3)}.')
