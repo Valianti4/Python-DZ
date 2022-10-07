2eeec0057404a0577d66b21815f1d4da22bbcc46
Valianti4 <109241884+Valianti4@users.noreply.github.com>
2022-10-07 19:38:17 +0400

    Решил задачу 4

--- Practice04/Example04.py
+++ Practice04/Example04.py
@@ -7,41 +7,24 @@ k=2 => 2*x^2 + 4*x + 5 = 0 или x^2 + 5 = 0 или 10*x^2 = 0
 '''
 
 
-from random import randint
+from random import randint, choice
 
-print('Введите натуральную степень k:')
+print('Чтобы записать в файл многочлен степени k, введите целое число:')
 k = int(input())
 
-def write_file(st):
-    with open('Task04.txt', 'w') as data:
-        data.write(st)
+def get_string(num: int):
+    line = ''
+    signs = ['+ ', '- ']
+    while num:
+        occ = randint(0, 100)
+        if occ:
+            line += f'{occ} * x ^ {num} '
+            line += choice(signs)
+        num -= 1
+    line += f'{randint(0, 100)} = 0'
+    return line
 
-def create_list(k):
-    list = []
-    for i in range(k + 1):
-        list.append(randint(0, 101))    
-    return list
-    
-def create_str(sp):
-    list = sp[::-1]
-    wr = ''
-    if len(list) < 1:
-        wr = 'x = 0'
-    else:
-        for i in range(len(list)):
-            if i != len(list) - 1 and list[i] != 0 and i != len(list) - 2:
-                wr += f'{list[i]}x^{len(list) - i - 1}'
-                if list [i + 1] != 0:
-                    wr += ' + '
-            elif i == len(list) - 2 and list[i] != 0:
-                wr += f'{list[i]}x'
-                if list[i + 1] != 0:
-                    wr += ' + '
-            elif i == len(list) - 1 and list[i] != 0:
-                wr += f'{list[i]} = 0'
-            elif i == len(list) - 1 and list[i] == 0:
-                wr += ' = 0'
-    return wr
-
-koef = create_list(k)
-write_file(create_str(koef))
+with open('Task04.txt', "w",) as file:
+    if k > 0:
+        file.write(get_string(k))
+    file.close()
